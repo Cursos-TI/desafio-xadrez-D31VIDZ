06f8efe64af34ac56d1fcf548d83a7cbb14f295c
Deivid Zioto <142805490+D31VIDZ@users.noreply.github.com>
2026-03-28 00:42:40 +0000

    foi implementado funções recursivas loops avançados e complexos

--- xadrez.c
+++ xadrez.c
@@ -4,79 +4,151 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+//função para validar qual a direção da peça
+char* Direcao(int num){
+
+    if(num == 1) return "cima\n";
+    if(num == 2) return "baixo\n";
+    if(num == 3) return "direita\n";
+    return "esquerda\n";
+}
+
+char* DirecaoDiagonal(int num){
+
+    if(num == 1) return "Cima, direita\n";
+    if(num == 2) return "Cima, esquerda\n";
+    if(num == 3) return "baixo, direita\n";
+    return "baixo, esquerda\n";
+}
+
+//função da movimentação da Torre
+void moviTorre(int casas, int direcao){
+
+    for (int i = 0; i < casas; i++){
+        printf("%s", Direcao(direcao));
+    }   
+}
+
+void moviBispo(int casas, int direcao){
+
+    for (int i = 0; i < casas; i++) {
+        printf("%s", DirecaoDiagonal(direcao));
+    }
+    
+}
+
+//função recursivo
+void moviRainha(int casas, int direcao){
+
+    if(casas < 1){
+        return;
+    }
+
+    printf("%s", Direcao(direcao));
+    moviRainha(casas - 1, direcao);
+}
+
+void moviCavalo(int vertical, int horizontal){
+
+    for (int i = 0; i < 2; i++) {
+        printf("%s\n", vertical == 1 ? "cima" : "Baixo");
+    }
+    printf("%s\n", horizontal == 1 ? "direita" : "esquerda");
+}
+
 int main() {
-    // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
     //Jogo Xadrez com três peças torre, Bispo e rainha
-    int DTorre, DBispo, DRainha, Dcavalo;
-    int torre = 0, bispo = 0, rainha, cavalo = 0;
-
-    //Movimento da torre onde o usuario fala a direção.
-    printf("** Direções da torre **\n");
-    printf("1. Cima\n");
-    printf("2. Baixo\n");
-    printf("3. Direita\n");
-    printf("4. Esquerda\n");
-    printf("Digite a direcao para a torre mover 5 casas: ");
-    scanf("%d", &DTorre);
-
-    printf("\n Movimento da torre: \n");
-    while (torre < 5) {
-        if (DTorre == 1){
-            printf("Cima\n");
-            torre++;
-        }else if(DTorre == 2){
-            printf("Baixo\n");
-            torre++;           
-        }else if(DTorre == 3){
-            printf("Direita\n");
-            torre++;
-        }else if (DTorre == 4) {
-            printf("Esquerda\n");
-            torre++;
-        }else{
-            printf("opcao invalida\n");
-            printf("Digite novamente: ");
+    int DTorre, DBispo, DRainha, DChorizontal, DCvertical;
+    int casaTorre, casaBispo, casaRainha, casaCavalo;
+    
+    //Movimento da torre
+    do{
+        printf("** Direções da torre **\n");
+        printf("1. Cima\n");
+        printf("2. Baixo\n");
+        printf("3. Direita\n");
+        printf("4. Esquerda\n");
+        printf("Digite a direcao em que a torre vai mover: ");
             scanf("%d", &DTorre);
-        }
-    }
 
-    // Movimento do bispo 5 casas em diagonal para cima e direita
-    printf("\n Movimento do Bispo: \n");
+            if(DTorre < 1 || DTorre > 4){
+                printf("opcao invalida\n");
+            }
+    }while (DTorre < 1 || DTorre > 4);
+
+    printf("Quantas casas a torre vai andar: ");
+    scanf("%d", &casaTorre);
+
+    moviTorre(casaTorre, DTorre);
+
+    // Movimento do bispo em diagonal 
     do
     {      
-        printf("cima, direita \n");
-        bispo++;
-    } while (bispo < 5);
-
-    //Movimento da rainha 8 casas para a esquerda
-    printf("\n Movimento da rainha 8 casas para a esquerda \n");
-    for (rainha = 0; rainha < 8; rainha++){
-        printf("esquerda \n");
+    printf("** Direcao do bispo em diagonal ** \n");
+    printf("1. Cima, direita\n");
+    printf("2. cima, esquerda\n");
+    printf("3. baixo, direita\n");
+    printf("4. baixo, esquerda\n");
+    printf("Digite qual direção o bispo vai mover: ");
+    scanf("%d", &DBispo); 
+    
+    if(DBispo < 1 || DBispo > 4){
+        printf("opção invalida\n");
     }
+    } while (DBispo < 1 || DBispo > 4);
+
+    printf("Quantas casas o bipo vai andar: ");
+    scanf("%d", &casaBispo); 
+
+    moviBispo(casaBispo, DBispo);
+
+    do {   
+        //Movimento da rainha
+        printf("** Direções da Rainha **\n");
+        printf("1. Cima\n");
+        printf("2. Baixo\n");
+        printf("3. Direita\n");
+        printf("4. Esquerda\n");
+        printf("Digite a direcao em que a rainha vai mover: ");
+            scanf("%d", &DRainha);
+        
+        if (DRainha < 1 || DRainha > 4) {
+            printf("invalido! \n");
+        }
+    }while (DRainha < 1 || DRainha > 4);
+
+    printf("Quantas casas a rainha vai andar: ");
+        scanf("%d", &casaRainha);
+    moviRainha(casaRainha, DRainha);
 
         //Movimento do cavalo
-    printf("** Direçao do cavalo **\n");
-    printf("1. Direita\n");
-    printf("2. Esquerda\n");
-    printf("Digite a direcao do cavalo: ");
-    scanf("%d", &Dcavalo);
+    
+    do {
+        printf("** Direçao do cavalo horizontal**\n");
+        printf("1. Direita\n");
+        printf("2. Esquerda\n");
+        printf("Digite a direcao do cavalo: ");
+        scanf("%d", &DChorizontal);
 
-    for (int i = 0; i < 1; i++) {
-        printf("Baixo, Baixo \n");
-        while (cavalo < 1){
-            if (Dcavalo == 1) {
-                printf("Direita");
-                cavalo++;
-            }else if (Dcavalo == 2) {
-                printf("Esquerda \n");
-                cavalo++;
-            }else {
-            printf("opcao invalida\n");
-            printf("Digite novamente: ");
-            scanf("%d", &Dcavalo);  
-            } 
+        if (DChorizontal < 1 || DChorizontal > 4) {
+            printf("invalido! \n");
         }
-    }
+    }while (DChorizontal < 1 || DChorizontal > 2);
+    
+    do {
+        printf("** Direçao do cavalo vertical**\n");
+        printf("1. cima\n");
+        printf("2. baixo\n");
+        printf("Digite a direcao do cavalo: ");
+        scanf("%d", &DCvertical);
+
+        if (DCvertical < 1 || DCvertical > 4) {
+            printf("invalido! \n");
+        }
+    }while (DCvertical < 1 || DCvertical > 2);
+
+    moviCavalo(DCvertical, DChorizontal);
+
     return 0;
 }
